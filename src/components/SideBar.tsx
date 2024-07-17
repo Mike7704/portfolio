10596b928a46ef79d0d2deba4e7257cc07294f27
Mike7704 <Michael.cowley2001@gmail.com>
2024-07-17 23:40:30 +0100

    Added box icons and more CSS

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,22 +1,36 @@
 import Link from "next/link";
+import Image from "next/image";
+import "boxicons/css/boxicons.min.css";
+import "@/styles/sidebar.css";
 
 export default function SideBar() {
   return (
     <div className="nav-sidebar">
-      <h2 className="title-name">Michael Cowley</h2>
+      <div className="profile-header">
+        <Image className="profile-pic" src={"/profile-pic.png"} width={256} height={256} alt="profile image" />
+        <h2 className="profile-name">Michael Cowley</h2>
+      </div>
       <nav>
         <ul>
           <li>
-            <Link href="#home">Home</Link>
+            <Link href="#home">
+              <i className="bx bx-home"></i> Home
+            </Link>
           </li>
           <li>
-            <Link href="#about">About</Link>
+            <Link href="#about">
+              <i className="bx bx-user"></i>About
+            </Link>
           </li>
           <li>
-            <Link href="#projects">Projects</Link>
+            <Link href="#projects">
+              <i className="bx bx-code-alt"></i>Projects
+            </Link>
           </li>
           <li>
-            <Link href="#contact">Contact</Link>
+            <Link href="#contact">
+              <i className="bx bx-envelope"></i>Contact
+            </Link>
           </li>
         </ul>
       </nav>
